f570a76a333b4e55e2d765634933d311a07e95fe
m <mauro81@gmail.com>
2019-09-03 10:34:27 +0200

    creazione tabella

--- app/src/main/java/com/example/rubricafilm/DBManager.java
+++ app/src/main/java/com/example/rubricafilm/DBManager.java
@@ -7,6 +7,10 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 
 public class DBManager extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "db";
+    public static final int DATABASE_VERSION = 1;
+
     public DBManager(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
@@ -14,6 +18,9 @@ public class DBManager extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
 
+        String query = "CREATE TABLE films (id INTEGER PRIMARY KEY AUTOINCREMENT , nome TEXT, genere TEXT, anno_prod TEXT, tipo_supp TEXT, locandina TEXT)";
+
+        sqLiteDatabase.execSQL(query);
     }
 
     @Override
